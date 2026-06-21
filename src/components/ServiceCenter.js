f89211dcce9583512e6840c6ce407f73df419a91
Ric Dodong <ricalgen@gmail.com>
2026-06-21 13:37:47 +0800

    Update ServiceCenter.js

--- src/components/ServiceCenter.js
+++ src/components/ServiceCenter.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const BASE_URL = 'https://dpsapi.ricalgen.eu.org';
+
 const ServiceCenter = () => {
     const [tickets, setTickets] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
