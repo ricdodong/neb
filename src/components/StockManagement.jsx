4f95db7f5b2646f64fbd1b995ab3de612bd7809b
Ric Dodong <ricalgen@gmail.com>
2026-08-16 09:46:36 +0800

    Update StockManagement.jsx

--- src/components/StockManagement.jsx
+++ src/components/StockManagement.jsx
@@ -680,7 +680,7 @@ const StockManagement = () => {
                                     </div>
                                     <div className="col-12 col-lg-5 bg-dark border-start border-secondary p-4 d-flex flex-column justify-content-between" style={{ fontSize: '12px' }}>
                                         <div className="d-flex flex-column gap-2 bg-black p-3 rounded border border-secondary">
-                                            
+
                                             <div className="d-flex justify-content-between border-bottom border-secondary pb-1.5">
                                                 <span className="text-secondary">DESCRIPTION:</span>
                                                 <span className="text-success fw-bold">{selectedLedgerEntry.parentItem?.item_description}</span>
@@ -694,10 +694,17 @@ const StockManagement = () => {
                                                 <span className="text-white">{selectedLedgerEntry.source}</span>
                                             </div>
                                             <div className="d-flex justify-content-between border-bottom border-secondary pb-1.5">
-                                                <span className="text-secondary">FREIGHT:</span>
+                                                <span className="text-secondary">FORWARD BY:</span>
+                                                <span className="text-success fw-bold">{selectedLedgerEntry.forwardedBy}</span>
+                                            </div>
+                                            <div className="d-flex justify-content-between border-bottom border-secondary pb-1.5">
+                                                <span className="text-secondary">FREIGHT COST:</span>
                                                 <span className="text-success">{formatCurrency(selectedLedgerEntry.freightCost || selectedLedgerEntry.shipping_cost)}</span>
                                             </div>
-
+                                            <div className="d-flex justify-content-between border-bottom border-secondary pb-1.5">
+                                                <span className="text-secondary">W/S PRICE:</span>
+                                                <span className="text-success">{formatCurrency(selectedLedgerEntry.wsPrice || selectedLedgerEntry.ws_price)}</span>
+                                            </div>
                                             <div className="d-flex justify-content-between border-bottom border-secondary pb-1.5">
                                                 <span className="text-secondary">SRP:</span>
                                                 <span className="text-success">{formatCurrency(selectedLedgerEntry.SRP || selectedLedgerEntry.srp_amount)}</span>
