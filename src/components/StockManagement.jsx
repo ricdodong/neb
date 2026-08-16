f162672ae6c40129f5b704b27750b14864567d4f
Ric Dodong <ricalgen@gmail.com>
2026-08-16 09:40:35 +0800

    Update StockManagement.jsx

--- src/components/StockManagement.jsx
+++ src/components/StockManagement.jsx
@@ -680,6 +680,11 @@ const StockManagement = () => {
                                     </div>
                                     <div className="col-12 col-lg-5 bg-dark border-start border-secondary p-4 d-flex flex-column justify-content-between" style={{ fontSize: '12px' }}>
                                         <div className="d-flex flex-column gap-2 bg-black p-3 rounded border border-secondary">
+                                            
+                                            <div className="d-flex justify-content-between border-bottom border-secondary pb-1.5">
+                                                <span className="text-secondary">DESCRIPTION:</span>
+                                                <span className="text-success fw-bold">{selectedLedgerEntry.parentItem?.item_description}</span>
+                                            </div>
                                             <div className="d-flex justify-content-between border-bottom border-secondary pb-1.5">
                                                 <span className="text-secondary">QTY:</span>
                                                 <span className="text-success fw-bold">{selectedLedgerEntry.qty}</span>
